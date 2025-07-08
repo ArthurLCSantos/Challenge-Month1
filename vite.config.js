67abdc7e30b1b703b228e91c68749beb0c67ffbd
ArthurLCSantos <catce.2022111isinf0546@aluno.ifpi.edu.br>
2025-07-08 09:29:36 -0300

    Refactor week2 project structure: remove unused files, update index.html to load assets directly, and adjust Vite configuration for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Challenge-Month-1/', // base correta para GitHub Pages
+  base: '/Challenge-Month1/', // base correta para GitHub Pages
   build: {
     outDir: 'dist',
   },
