058f8057f983f2d72a1ee49a0342e4a7be05c867
ArthurLCSantos <catce.2022111isinf0546@aluno.ifpi.edu.br>
2025-07-08 20:33:41 -0300

    fix: update asset paths in index.html and vite.config.js for consistency feat: add script-static.js for enhanced functionality in week1 project

--- week2/vite.config.js
+++ week2/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Challenge-Month-1/week2/', // importante para GitHub Pages
+  base: '/Challenge-Month1/week2/',  // caminho que será usado no GitHub Pages
   build: {
-    outDir: 'dist', // padrão
+    outDir: '../docs/week2',         // saída do build
+    emptyOutDir: true,               // limpa a pasta antes
   },
   plugins: [
     react(),
