8c933feb16ce622e85adedf373296f9a0ba3de46
ArthurLCSantos <catce.2022111isinf0546@aluno.ifpi.edu.br>
2025-07-08 09:17:43 -0300

    fix: update OrbitControls import to use CDN for consistency

--- docs/week1/script.js
+++ docs/week1/script.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.158.0/build/three.module.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.158.0/examples/jsm/controls/OrbitControls.js';
 
 //CREATING INPUTS CONSTANTS AND VELOCITY VARIABLE
 const canvas            =  document.querySelector("#C");
